f5a9ba7d792d746e5bdb6814cbe5dbb9420a497c
mahela@creatria.app <mahelaLks>
2025-08-10 22:41:20 +0530

    spring first commit

---
+++ src/main/java/com/mahelacode/todos/config/JwtAuthenticationFilter.java
@@ -0,0 +1,70 @@
+package com.mahelacode.todos.config;
+
+
+import com.mahelacode.todos.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.NonNull;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+
+
+    private final JwtService jwtService;
+
+    private final UserDetailsService userDetailsService;
+
+    public JwtAuthenticationFilter(JwtService jwtService,@Lazy UserDetailsService userDetailsService){ // delay untill need this
+        this.jwtService=jwtService;
+        this.userDetailsService=userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+          @NonNull HttpServletRequest request,
+          @NonNull  HttpServletResponse response,
+          @NonNull  FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail); //loads from db, inside spring security
+
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken( //creating user to spring security
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authToken); //set user to context
+            }
+        }
+        filterChain.doFilter(request, response); //like next() , continue chain if even valid or not
+    }
+}
---
+++ src/main/java/com/mahelacode/todos/entity/Authority.java
@@ -0,0 +1,23 @@
+package com.mahelacode.todos.entity;
+
+import jakarta.persistence.Embeddable;
+import org.springframework.security.core.GrantedAuthority;
+
+
+@Embeddable
+public class Authority implements GrantedAuthority {
+
+    private String authority;
+
+    public Authority(){
+    }
+
+    public Authority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        return null;
+    }
+}
